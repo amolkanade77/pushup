0abdc6de4f2fb18db17e2c601a4d1d65350570d6
amolkanade77 <80120632+amolkanade77@users.noreply.github.com>
2022-09-08 13:22:42 +0530

    Update app.py
    testt

--- app.py
+++ app.py
@@ -6,3 +6,25 @@ app = Flask(__name__)
 @app.route("/new")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
+ 
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    
+    print(request.values.get)
+    Finaldata=incoming_msg.splitlines()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'shopname' in incoming_msg:      
+        new_user = Profile(shopname=Finaldata[0].split('-')[1],Orderone=Finaldata[1].split('-')[1],ornumber=Finaldata[2].split('-')[1],last_name=Finaldata[3].split('-')[1],age=Finaldata[4].split('-')[1])
+        db.session.add(new_user)
+        db.session.commit()
+        quote="Your order saved SucessFully"
+        msg.body(quote)
+        responded = True
+    if not responded:
+        msg.body('Please place order!')
+    return str(resp)
