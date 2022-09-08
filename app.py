f0d6a2f3907bfb4bbd105fb55ab32090007ced12
amolkanade77 <80120632+amolkanade77@users.noreply.github.com>
2022-09-08 15:01:26 +0530

    Update app.py
    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
  
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql-fitted-59974'
 db = SQLAlchemy(app)
 
