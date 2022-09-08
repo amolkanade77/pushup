44e0c77183f061f943b2fb6eb2d1c6054dd117db
amolkanade77 <80120632+amolkanade77@users.noreply.github.com>
2022-09-08 12:14:10 +0530

    Update app.py
    changes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from pushups_logger import create_app
-
+from flask import request
+from flask import jsonify
 app = create_app()
+
+@app.route('/hello', methods=['GET'])
+def helloworld():
+    if(request.method == 'GET'):
+        data = {"data": "Hello World"}
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
